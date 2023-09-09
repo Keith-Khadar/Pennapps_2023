151d51be5dba498723ccab36b94d01eceede8a1d
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-09-09 12:54:58 -0400

    Fixed some stuff

--- StudyHedge/src/App.jsx
+++ StudyHedge/src/App.jsx
@@ -28,9 +28,82 @@ function App() {
       {!user ? (
         <Login></Login>
       ) : (
-        <div>
-          <Calendar></Calendar>
-          <Settings></Settings>
+        <div class="h-screen flex flex-col">
+          <div className="flex flex-1">
+            <main class="w-3/4 p-4">
+              <div class="h-full bg-white rounded-lg p-4 shadow">
+                <Calendar></Calendar>
+              </div>
+            </main>
+            <aside class="w-1/4 h-screen">
+              <div class="flex flex-col space-y-10 justify-around p-6">
+                <div class="flex justify-around items-center content-around">
+                  <div class="text-4xl font-bold text-blue-500 shadow-lg p-4 rounded-lg">
+                    Study Hedge
+                  </div>
+                </div>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-lg flex items-center">
+                  {/* SVG icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    className="w-8 h-8 mr-2" // Adjust the size and margin as needed
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  Add Deadline
+                </button>
+
+                <button className="bg-red-500 hover:bg-red-600 text-white py-3 px-6 rounded-lg flex items-center">
+                  {/* SVG icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    className="w-6 h-6 mr-2" // Adjust the size and margin as needed
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                    />
+                  </svg>
+                  Add Exam
+                </button>
+
+                <button className="bg-yellow-500 hover:bg-yellow-600 text-white py-3 px-6 rounded-lg flex items-center">
+                  {/* SVG icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    className="w-6 h-6 mr-2" // Adjust the size and margin as needed
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                    />
+                  </svg>
+                  Edit Schedule
+                </button>
+              </div>
+              <div className="absolute bottom-0 right-0 p-4">
+                <Settings></Settings>
+              </div>
+            </aside>
+          </div>
         </div>
       )}
     </div>
--- StudyHedge/src/components/calendar.jsx
+++ StudyHedge/src/components/calendar.jsx
@@ -9,59 +9,10 @@ import interactionPlugin from "@fullcalendar/interaction";
 import { INITIAL_EVENTS, createEventId } from "./event-utils";
 
 export const Calendar = () => {
-  const handleDateSelect = (selectInfo) => {
-    let title = prompt("Please enter a new title for your event");
-    let calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect(); // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
-    }
-  };
-
-  const handleEventClick = (clickInfo) => {
-    if (
-      confirm(
-        `Are you sure you want to delete the event '${clickInfo.event.title}'`
-      )
-    ) {
-      clickInfo.event.remove();
-    }
-  };
-
-  const handleEvents = (events) => {
-    this.setState({
-      currentEvents: events,
-    });
-  };
-
-  function renderEventContent(eventInfo) {
-    return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </>
-    );
-  }
   return (
     <FullCalendar
-      editable={true}
-      selectable={true}
-      selectMirror={true}
-      dayMaxEvents={true}
-      initialEvents={INITIAL_EVENTS}
-      select={handleDateSelect}
-      eventContent={renderEventContent}
-      eventClick={handleEventClick}
-      eventsSet={handleEvents}
-      plugins={[timeGridPlugin, interactionPlugin]}
+      plugins={[timeGridPlugin]}
+      contentHeight={"70vh"}
       initialView="timeGridWeek"
       headerToolbar={{
         left: "",
