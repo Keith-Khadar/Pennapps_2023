6762cc5693a67b033d6167ef593b04a6dd019638
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-11-19 16:41:42 -0500

    Started Work on the modal

--- StudyHedge/study-hedge/src/app/modal/deadline/deadline.component.ts
+++ StudyHedge/study-hedge/src/app/modal/deadline/deadline.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent, IonIcon, IonDatetime, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-deadline',
   templateUrl: './deadline.component.html',
   styleUrls: ['./deadline.component.scss'],
+  standalone: true,
+  imports: [IonContent, IonDatetime, IonIcon, IonGrid, IonRow],
 })
 export class DeadlineComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({addOutline});
+   }
 
   ngOnInit() {}
 
