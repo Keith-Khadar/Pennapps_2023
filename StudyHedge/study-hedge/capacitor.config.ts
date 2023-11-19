3f0f28a3f14086d55731282bada91c281ac0f212
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-11-19 14:18:56 -0500

    Ionic + Angular Init
    Setup Ionic and Angular. Removed React

---
+++ StudyHedge/study-hedge/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Study Hedge',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
